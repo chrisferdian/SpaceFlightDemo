2382af68e5c131247efa46384664d0b243e7f6bd
Chris Ferdian <chris.ferdian@gmail.com>
2024-10-02 05:57:31 +0700

    (feat) added timer login

---
+++ SpaceFlightDemo/Presentation/EmptyStateView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyStateView.swift
+//  SpaceFlightDemo
+//
+//  Created by Indo Teknologi Utama on 01/10/24.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyStateView()
+}
--- SpaceFlightDemo/Presentation/Splash/SplashView.swift
+++ SpaceFlightDemo/Presentation/Splash/SplashView.swift
@@ -1,18 +1,18 @@
 //
-//  AuthView.swift
+//  SplashView.swift
 //  SpaceFlightDemo
 //
-//  Created by Indo Teknologi Utama on 27/09/24.
+//  Created by Indo Teknologi Utama on 01/10/24.
 //
 
 import SwiftUI
 
-struct AuthView: View {
+struct SplashView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AuthView()
+    SplashView()
 }
